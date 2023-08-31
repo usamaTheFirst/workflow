2c3c3240566a608e4b65bd30a4ca1f48058d8f2a
Usama <72226908+usamaTheFirst@users.noreply.github.com>
2023-08-31 10:05:22 +0500

    added ignored files

---
+++ SAPConnection/Data/DbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace SAPConnection.Data
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+            //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=CustomAppsDatabase;User ID=sa;Password=Appdev@786#;Persist Security Info=False;Integrated Security=False;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = CustomAppsDatabase; Trusted_Connection = True; MultipleActiveResultSets = true");
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
